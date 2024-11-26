6ed3a3a662c420e079834ff230106cfa9a227452
Zakaria Asad <zasad2587@gmail.com>
2024-11-26 19:38:13 +0200

    Build expandable featured component

--- sections/Featured.tsx
+++ sections/Featured.tsx
@@ -1,4 +1,5 @@
 import FeaturedCard from "@/components/cards/FeaturedCard";
+import ExpandableFeatured from "@/components/expandable/ExpandableFeatured";
 import Heading from "@/components/heading/Heading";
 
 const FeaturedSection = () => {
@@ -6,6 +7,9 @@ const FeaturedSection = () => {
     <div className="pt-24 px-3 lg:px-8">
       <Heading number="01" title1="Featured" title2="Work" />
       <FeaturedCard active tag="Any" title="Any" video="" />
+      <div className="mt-24">
+        <ExpandableFeatured />
+      </div>
     </div>
   );
 };
