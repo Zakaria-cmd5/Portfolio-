78c5808b7ba33b0883ba5a65708136eb1ab4f9aa
Zakaria Asad <zasad2587@gmail.com>
2024-11-28 14:24:19 +0200

    Build Contact Me From

--- sections/ContactMe.tsx
+++ sections/ContactMe.tsx
@@ -1,8 +1,12 @@
 import ContactMeCard from "@/components/cards/ContactMe";
 import Heading from "@/components/heading/Heading";
+import Button from "@/components/ui/Button";
 import Card from "@/components/ui/Card";
-import { FaPhoneVolume } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
+import Input from "@/components/ui/Input";
+import TextArea from "@/components/ui/TextArea";
+import { FaPhoneVolume, FaProjectDiagram, FaUser } from "react-icons/fa";
+import { MdEmail, MdSubject } from "react-icons/md";
+import { SiMinutemailer } from "react-icons/si";
 
 const ContactMeSection = () => {
   return (
@@ -23,6 +27,38 @@ const ContactMeSection = () => {
               title="Chat with me directly at"
               icon={<MdEmail className="fill-[#333] text-lg" />}
             />
+          </div>
+          <div className="lg:col-span-2 bg-secondary-background border shadow-md border-border space-y-6 relative overflow-hidden px-[25px] py-5">
+            <div className="flex flex-col lg:flex-row items-center justify-between mb-4 gap-8">
+              <Input placeholder="Full Name" type="text" icon={<FaUser />} />
+              <Input
+                placeholder="Email Address"
+                type="email"
+                icon={<MdEmail />}
+              />
+            </div>
+            <div className="flex items-center justify-between mb-4 gap-8">
+              <Input placeholder="Subject" type="text" icon={<MdSubject />} />
+            </div>
+            <div className="flex flex-col gap-6">
+              <div>
+                <h1 className="font-bold text-lg">
+                  What services are you in need for ?
+                </h1>
+                <div className="flex flex-wrap items-center justify-between mb-4 gap-8">
+                  {/* Options */}
+                </div>
+              </div>
+            </div>
+            <TextArea
+              placeholder="Tell Me What You want From Me"
+              icon={<FaProjectDiagram />}
+            />
+            <div className="w-full flex justify-end">
+              <Button classNames="!w-44 !py-3 !text-xl">
+                Send <SiMinutemailer />
+              </Button>
+            </div>
           </div>
         </div>
       </Card>
